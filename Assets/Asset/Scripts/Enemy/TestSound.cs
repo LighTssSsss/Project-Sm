0776563294c9b3c0e00c9a4a2edc0f9b199b5b14
LighTssSsss <gonzalmartinez2014@gmail.com>
2024-04-11 17:34:01 -0400

    IA sound detection avance 1

---
+++ Assets/Asset/Scripts/Enemy/TestSound.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GamePlay
+{
+    public class TestSound : MonoBehaviour
+    {
+        [SerializeField] private AudioSource source = null;
+
+        [SerializeField] private float soundRange;
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            if (source.isPlaying)
+            {
+                return;
+            }
+
+            source.Play();
+
+            var sound = new SoundChecker(transform.position, soundRange);
+
+            Debug.Log("Sound wit position: " + (sound.pos) + "and range: " + (sound.ranges));
+            sound.soundType = SoundChecker.SoundType.Insteresting;
+            SoundsCh.MakeSound(sound);
+        }
+    }
+}
+
