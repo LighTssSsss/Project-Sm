1e70321ce2adb5e3f0e1e4e805301d197bcfb77d
LighTssSsss <gonzalmartinez2014@gmail.com>
2024-07-13 23:16:37 -0400

    Mejoras

---
+++ Assets/Cinematicas/VuelveMenu.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class VuelveMenu : MonoBehaviour
+{
+    [SerializeField] private float forwardOffset = 1;
+    [SerializeField] private float verticalOffset = 0;
+    [SerializeField] private Vector3 size = Vector3.one;
+    [SerializeField] private LayerMask layerMask;
+    public PlayableDirector cinematica;
+
+    public float tiempoApaga;
+    public bool poseeCinematica;
+    public bool tengoCinematica = true;
+    private bool CinematicaListaPlayer;
+    private bool puedoR;
+
+    public GameObject objetos;
+    public GameObject texto;
+    public Animator textoD;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        texto.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReproduceCinematicas();
+
+        if (Input.GetKeyDown(KeyCode.E) && puedoR == true)
+        {
+            cinematica.Play();
+            StartCoroutine(Apaga());
+            StartCoroutine(ApagaObjeto());
+        }
+    }
+
+
+
+    private void ReproduceCinematicas()
+    {
+        Vector3 center = transform.position + (transform.forward * forwardOffset);
+        center.y += verticalOffset;
+
+        Collider[] colliders = Physics.OverlapBox(center, size, transform.rotation, layerMask);
+
+        foreach (Collider player in colliders)
+        {
+            /*
+            DataManager.data.posicionPlayer = true;
+            DataManager.data.posicion = this.transform.position;
+            DataManager.data.posicion = this.transform.position;*/
+            puedoR = true;
+            //cinematica.Play();
+            Debug.Log("Puedo iniciar la cinematica");
+
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            texto.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            //ReproduceTexto("InteractuarDesaparece");
+            texto.SetActive(false);
+        }
+
+    }
+
+    public void ReproduceTexto(string nombreA)
+    {
+        textoD.Play(nombreA);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Vector3 center = transform.position + (transform.forward * forwardOffset);
+        center.y += verticalOffset;
+
+        Gizmos.DrawWireCube(center, size);
+    }
+
+
+    IEnumerator Apaga()
+    {
+        yield return new WaitForSeconds(tiempoApaga);
+        SceneManager.LoadScene(0);
+       // this.gameObject.SetActive(false);
+        if (cinematica != null)
+        {
+            cinematica.enabled = false;
+            Debug.Log("Apaga");
+
+        }
+
+        if (poseeCinematica)
+        {
+            CinematicaListaPlayer = false;
+            // DataManager.data.cinematicaTermino = CinematicaListaPlayer; // Este
+            // Debug.Log("No se debe reproducir");
+        }
+
+
+
+    }
+
+    IEnumerator ApagaObjeto()
+    {
+        yield return new WaitForSeconds(2);
+        objetos.SetActive(false);
+
+
+
+
+    }
+
+    public void Termina()
+    {
+        if (CinematicaListaPlayer)
+        {
+            double duracionTotal = cinematica.duration;
+            //musica.SetActive(true);
+            cinematica.time = duracionTotal;
+
+            cinematica.playableGraph.Evaluate();
+            cinematica.enabled = false;
+
+
+        }
+
+    }
+}
